427440bbb24f4f996b95d855f11a04e8db33a37e
Martin Bárta <martin.barta@hotmail.com>
2017-01-04 14:30:18 +0100

    Allow shallow clone of Boost.

--- download-libraries.sh
+++ download-libraries.sh
@@ -14,8 +14,19 @@ git clone --depth 1 https://github.com/zeux/pugixml.git
 git clone --depth 1 https://github.com/google/googletest.git unit-test/googletest
 
 git clone --depth 1 https://github.com/boostorg/boost.git
-cd boost # TODO: figure out how to do a shallow clone of the submodules
-git submodule update --init -- libs/program_options libs/config libs/any libs/type_index libs/static_assert libs/throw_exception libs/assert libs/core libs/type_traits libs/mpl libs/preprocessor libs/function libs/integer libs/bind libs/move libs/lexical_cast libs/range libs/iterator libs/detail libs/concept_check libs/utility libs/numeric/conversion libs/array libs/functional libs/container libs/math libs/predef libs/smart_ptr libs/tokenizer
-cp -r libs/*/include/boost libs/*/*/include/boost .
+LIBS="libs/program_options libs/config libs/any libs/type_index libs/static_assert libs/throw_exception libs/assert libs/core libs/type_traits libs/mpl libs/preprocessor libs/function libs/integer libs/bind libs/move libs/lexical_cast libs/range libs/iterator libs/detail libs/concept_check libs/utility libs/numeric/conversion libs/array libs/functional libs/container libs/math libs/predef libs/smart_ptr libs/tokenizer"
+cd boost
+GIT_V1=`git --version | sed -r 's/.*([1-9])[.][1-9]*[.].*/\1/'`
+GIT_V2=`git --version | sed -r 's/.*[1-9][.]([1-9]*)[.].*/\1/'`
+if [ $GIT_V1 -ge 2 ]
+then
+	if [ $GIT_V2 -ge 8 ]
+	then
+		git submodule update --init --depth 1 -j 4 -- $LIBS
+	else
+		git submodule update --init -- $LIBS
+	fi
+fi
+cp -rf libs/*/include/boost libs/*/*/include/boost .
 cd -
 

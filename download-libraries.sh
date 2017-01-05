b740a61b6e9e3836904396c4da97a6d48bd72f44
Martin Bárta <martin.barta@hotmail.com>
2017-01-05 10:54:53 +0100

    Add download summary.

--- download-libraries.sh
+++ download-libraries.sh
@@ -1,30 +1,70 @@
 #!/bin/bash
 
-#git clone --depth 1 https://github.com/mbillingr/libgdf.git
-git clone --depth 1 https://github.com/machta/libgdf.git -b update-gdf-v2.51
-sed -ri 's/(find_package.*)/#\1/' libgdf/libgdf/CMakeLists.txt
-sed -ri 's/(add_subdirectory.*tools.*)/#\1/' libgdf/CMakeLists.txt
+if [ -d libgdf ]
+then
+	libgdf=skipped
+else
+	#git clone --depth 1 https://github.com/mbillingr/libgdf.git &&
+	git clone --depth 1 https://github.com/machta/libgdf.git -b update-gdf-v2.51 &&
+	sed -ri 's/(find_package.*)/#\1/' libgdf/libgdf/CMakeLists.txt &&
+	sed -ri 's/(add_subdirectory.*tools.*)/#\1/' libgdf/CMakeLists.txt &&
+	libgdf=OK || libgdf=fail
+fi
 
-git clone --depth 1 https://github.com/Teuniz/EDFlib.git
-echo 'add_definitions(-D_LARGEFILE64_SOURCE -D_LARGEFILE_SOURCE)' > EDFlib/CMakeLists.txt
-echo 'add_library(EDF edflib.c)' >> EDFlib/CMakeLists.txt
+if [ -d EDFlib ]
+then
+	EDFlib=skipped
+else
+	git clone --depth 1 https://github.com/Teuniz/EDFlib.git &&
+	echo 'add_definitions(-D_LARGEFILE64_SOURCE -D_LARGEFILE_SOURCE)' > EDFlib/CMakeLists.txt &&
+	echo 'add_library(EDF edflib.c)' >> EDFlib/CMakeLists.txt &&
+	EDFlib=OK || EDFlib=fail
+fi
 
-git clone --depth 1 https://github.com/zeux/pugixml.git
+if [ -d pugixml ]
+then
+	pugixml=skipped
+else
+	git clone --depth 1 https://github.com/zeux/pugixml.git &&
+	pugixml=OK || pugixml=fail
+fi
 
-git clone --depth 1 https://github.com/google/googletest.git unit-test/googletest
+if [ -d unit-test/googletest ]
+then
+	googletest=skipped
+else
+	git clone --depth 1 https://github.com/google/googletest.git unit-test/googletest &&
+	googletest=OK || googletest=fail
+fi
 
-BOOST=boost_1_63_0
-curl -L https://sourceforge.net/projects/boost/files/boost/1.63.0/boost_1_63_0.zip > $BOOST.zip
-unzip -q $BOOST.zip
-rm $BOOST.zip
-mkdir -p boost/libs
-mv $BOOST/boost boost
-mv $BOOST/libs/program_options boost/libs
-rm -rf $BOOST
+if [ -d boost ]
+then
+	boost=skipped
+else
+	BOOST=boost_1_63_0 &&
+	curl -L https://sourceforge.net/projects/boost/files/boost/1.63.0/boost_1_63_0.zip > $BOOST.zip &&
+	unzip -q $BOOST.zip &&
+	rm $BOOST.zip &&
+	mkdir -p boost/libs &&
+	mv $BOOST/boost boost &&
+	mv $BOOST/libs/program_options boost/libs &&
+	rm -rf $BOOST &&
+	boost=OK || boost=fail
 
-# This version doesn't work in git-bash but is faster.
-#BOOST=boost_1_63_0
-#curl -L https://sourceforge.net/projects/boost/files/boost/1.63.0/boost_1_63_0.zip > $BOOST.zip
-#unzip -q $BOOST.zip $BOOST/boost/* $BOOST/libs/program_options/*
-#rm $BOOST.zip
-#mv $BOOST boost
+	# This version doesn't work in git-bash but is faster.
+	#BOOST=boost_1_63_0 &&
+	#curl -L https://sourceforge.net/projects/boost/files/boost/1.63.0/boost_1_63_0.zip > $BOOST.zip &&
+	#unzip -q $BOOST.zip $BOOST/boost/* $BOOST/libs/program_options/* &&
+	#rm $BOOST.zip &&
+	#mv $BOOST boost
+fi
+
+echo
+echo ========== Download summary ==========
+echo "Library                 Status"
+echo ======================================
+echo "libgdf                  $libgdf"
+echo "EDFlib                  $EDFlib"
+echo "pugixml                 $pugixml"
+echo "unit-test/googletest    $googletest"
+echo "boost                   $boost"
